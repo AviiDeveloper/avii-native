d6048fc64f78b54a1da5c05517d8592c70cb4c2c
Louis Grochla <Louis.grochla@icloud.com>
2025-11-17 15:27:04 +0000

    Add AviiNative project files

---
+++ AviiNative/AviiNative/AviiNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AviiNativeApp.swift
+//  AviiNative
+//
+//  Created by Avii Developer on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AviiNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
